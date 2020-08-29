3192605a76647114c7aa62ba5698b7e13bf1bbd8
gibbyfree <free.g@husky.neu.edu>
2020-08-29 18:46:54 -0400

    Updated comment scraper

--- scripts/comment_scraper.py
+++ scripts/comment_scraper.py
@@ -17,28 +17,38 @@ reddit = praw.Reddit(client_id='VDczLkMdXSQQdQ', \
                      client_secret='z1bRECrJer5qe_cGXu_Nf94xTcY', \
                      user_agent='py:scraper')
 
-# Create the (soon-to-be-massive) dictionary for storing each analyzed comment.
+# Create the (soon-to-be-massive) dictionary for storing each slur-containing comment.
 comment_dict = { "title": [], "score": [], "id": [], "body": [], "slurs": []}
+# Create the (soon-to-be-massive) dictionary for storing each slur's usage information.
+slur_use_dict = {"id": [], "slur_used": [], "slur_category": []}
 
 # Analyze each story.
 for story in stories_data:
     submission = reddit.submission(url=story['url'])
     submission.comments.replace_more(limit=0)
-    # Add basic info about each comment to the dictionary.
+    # Add basic info about each comment to the dictionary, if there are slurs in the comment.
     for comment in submission.comments.list():
-        comment_dict['title'].append(comment.submission.title)
-        comment_dict['score'].append(comment.score)
-        comment_dict['id'].append(comment.id)
-        comment_dict['body'].append(comment.body)
         # Check each comment for slurs.
         slur_count = 0
         for slur in slurs_data:
             if slur['term'] in comment.body:
                 slur_count = slur_count + 1
-        comment_dict['slurs'].append(slur_count)
+                slur_use_dict['id'].append(comment.id)
+                slur_use_dict['slur_used'].append(slur['term'])
+                slur_use_dict['slur_category'].append(slur['category'])
+        # Only add a comment to the dictionary if it contains at least one slur.
+        if slur_count > 0:
+            comment_dict['title'].append(comment.submission.title)
+            comment_dict['score'].append(comment.score)
+            comment_dict['id'].append(comment.id)
+            comment_dict['body'].append(comment.body)
+            comment_dict['slurs'].append(slur_count)
 
 
 # Export all of the data!
 comment_data = pandas.DataFrame(comment_dict)
 comment_data.to_csv('../data/comments.csv', index=False)
+
+slur_use_data = pandas.DataFrame(slur_use_dict)
+slur_use_data.to_csv('../data/slurs.csv', index=False)
         
